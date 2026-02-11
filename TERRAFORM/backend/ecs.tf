79a8dc6a0235bf5916b12c915cbd1a0ec8a2c376
MasterBhuvnesh <bhuvneshverma2904@gmail.com>
2026-02-12 03:59:34 +0530

    fix : file str

--- TERRAFORM/backend/ecs.tf
+++ TERRAFORM/backend/ecs.tf
@@ -1,17 +1,3 @@
-# ECR Repository
-resource "aws_ecr_repository" "repo" {
-  name                 = "${var.app_name}-repo"
-  image_tag_mutability = "MUTABLE"
-
-  image_scanning_configuration {
-    scan_on_push = true
-  }
-
-  tags = {
-    Name = "${var.app_name}-repo"
-  }
-}
-
 # ECS Cluster
 resource "aws_ecs_cluster" "cluster" {
   name = "${var.app_name}-cluster"
